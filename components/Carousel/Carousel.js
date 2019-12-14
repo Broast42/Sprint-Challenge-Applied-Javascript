9a0b7e3272a51972cacb5f6e5a9ecbea0f7a4cc5
rdowd <rjarrettdowd@yahoo.com>
2019-12-13 21:49:52 -0500

    tried to get stretch goal but did not get buttons to work in time.

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,65 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const carCont = document.querySelector(".carousel-container");
+
+carCont.appendChild(carousel());
+
+function carousel(){
+  const carDiv = document.createElement("div");
+  const leftDiv = document.createElement("div");
+  const rightDiv = document.createElement("div");
+  const mount = document.createElement("img");
+  const comp = document.createElement("img");
+  const tree = document.createElement("img");
+  const turn = document.createElement("img");
+
+  let index = 0;
+
+  carDiv.classList.add("carousel");
+  leftDiv.classList.add("left-button");
+  rightDiv.classList.add("right-button");
+  leftDiv.textContent = " < ";
+  rightDiv.textContent = " > ";
+  mount.src = "./assets/carousel/mountains.jpeg";
+  comp.src = "./assets/carousel/computer.jpeg";
+  tree.src = "./assets/carousel/trees.jpeg";
+  turn.src = "./assets/carousel/turntable.jpeg";
+
+  carDiv.appendChild(leftDiv);
+  carDiv.appendChild(mount);
+  carDiv.appendChild(comp);
+  carDiv.appendChild(tree);
+  carDiv.appendChild(turn);
+  carDiv.appendChild(rightDiv);
+
+  if(index === 0){
+    mount.style.display = "block";
+  }else if(index === 1){
+    comp.style.display = "block";
+  }else if(index === 2){
+    tree.style.display = "block";
+  }else if (index === 3){
+    turn.style.display = "block";
+  }
+
+  leftDiv.addEventListener("click", ()=>{
+    index - 1;
+    if (index < 0){
+      index = 3;
+    }
+  })
+
+  rightDiv.addEventListener("click", ()=>{
+    index + 1;
+    if (index > 3){
+      index = 0;
+    }
+  })
+
+  return carDiv;
+
+  
+
+}
